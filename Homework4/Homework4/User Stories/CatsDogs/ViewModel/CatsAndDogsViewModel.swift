27b36da6d47e80f13449836e65b8fb32b4efab59
EvgeniiSamarin <evgeniy.samarin@flatstack.com>
2021-11-08 16:12:34 +0300

    Create view and viewmodel

---
+++ Homework4/Homework4/User Stories/CatsDogs/ViewModel/CatsAndDogsViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  CatsAndDogsViewModel.swift
+//  Homework4
+//
+//  Created by Евгений Самарин on 02.11.2021.
+//
+
+import Foundation
+import Combine
+
+final class CatsAndDogsViewModel {
+
+    enum ContentType: Int {
+        case cats
+        case dogs
+    }
+
+    // MARK: - Instance Properties
+
+    @Published var score: ScoreModel = ScoreModel()
+    @Published var catsFact: String = ""
+    @Published var dogsImage: String = ""
+
+    var contentType: ContentType = .cats
+    let errorResult = PassthroughSubject<Error, Never>()
+
+    // MARK: -
+
+    private var factsSubscriber: AnyCancellable?
+    private var imagesSubscriber: AnyCancellable?
+
+    // MARK: - Instance Methods
+
+    func getContent() {
+        switch self.contentType {
+        case .cats:
+            self.getCatFact()
+
+        case .dogs:
+            self.getDogsImage()
+        }
+    }
+
+    func resetView() {
+        self.catsFact = ""
+        self.dogsImage = ""
+        self.score.dogsScore = 0
+        self.score.catsScore = 0
+    }
+
+    private func getCatFact() {
+        self.factsSubscriber = APIManager.shared.getRandomCatFact()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    self.errorResult.send(error)
+
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] response in
+                self?.catsFact = response.fact
+                self?.score.catsScore += 1
+            })
+    }
+
+    private func getDogsImage() {
+        self.imagesSubscriber = APIManager.shared.getRandomDogImage()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    self.errorResult.send(error)
+
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] response in
+                self?.dogsImage = response.message
+                self?.score.dogsScore += 1
+            })
+    }
+}
